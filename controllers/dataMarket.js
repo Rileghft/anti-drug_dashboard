d89cae1acb8fc31d8937fcb8016e1bf0cc5187d0
sleep <wjs09211>
2017-05-27 12:53:39 +0800

    static file

--- controllers/dataMarket.js
+++ controllers/dataMarket.js
@@ -1,17 +1,11 @@
 const views = require('koa-views')
 const mysql = require('mysql')
-const conifg = require('../config.js')
-let connection = mysql.createPool(conifg.db)
+const market = require('../models/dataMarket')
 
 let dataMarket = async(ctx) => {
-    connection.query('SELECT * FROM datalist',function(error, rows, fields){
-        //檢查是否有錯誤
-        if(error){
-            throw error;
-        }
-        console.log(rows[0].id); //2
-    });
-    await ctx.render('dashboard', title="test")
+
+    rows = await market.getDataList(null)
+    await ctx.render('dataMarket', rows=rows)
 }
 
 module.exports = dataMarket
--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -4,6 +4,26 @@
 
 const mysql = require('mysql')
 const conifg = require('../config.js')
-let connection = mysql.createConnection(conifg.db)
+const pool  = mysql.createPool(conifg.db)
 
-module.exports = connection
+let query = function( sql, values ) {
+    return new Promise(( resolve, reject ) => {
+        pool.getConnection(function(err, connection) {
+            if (err) {
+                reject( err )
+            } else {
+                connection.query(sql, values, ( err, rows) => {
+
+                    if ( err ) {
+                        reject( err )
+                    } else {
+                        resolve( rows )
+                    }
+                    connection.release()
+                })
+            }
+        })
+    })
+}
+
+module.exports = query
