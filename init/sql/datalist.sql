701a9ecd2b73029eaadc947a9d8546c383532b39
YuMin Hung <yuminhung@MacBook-Pro.local>
2017-05-27 11:31:34 +0800

    pull yumin

---
+++ init/sql/datalist.sql
@@ -0,0 +1,18 @@
+USE hackthon_data;
+DROP TABLE IF EXISTS datalist;
+CREATE TABLE datalist {
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(20) NOT NULL,
+	title VARCHAR(50) NOT NULL,
+	provider VARCHAR(100) DEFAULT 'unknown',
+	description TEXT NOT NULL,
+	dataType VARCHAR(10) NOT NULL,
+	dateFrom INT,
+	dateUnit VARCHAR(10) NOT NULL,
+	fee VARCHAR(5) NOT NULL,
+	PRIMARY KEY (id, name)
+};
+
+INSERT INTO datalist 
+("name", "title", "provider", "description", "dataType", "dateFrom", "dateUnit", "fee")
+VALUES("drugCriminal", "毒品案件辦理情", "法務部統計處", "法務部毒品案件辦理情況統計表", "json", "88", "民國", "free");
