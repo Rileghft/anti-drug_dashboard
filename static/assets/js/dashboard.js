56f6019ea86a2f6a0de3f89aafd9282b417a8596
sleep <wjs09211>
2017-05-27 23:56:01 +0800

    dashboard

--- static/assets/js/dashboard.js
+++ static/assets/js/dashboard.js
@@ -5,24 +5,7 @@
 
 $(function(){
 
-    function pieChart(id,  x, y){
-        var data = [{
-            values: [19, 26, 55],
-            labels: ['Residential', 'Non-Residential', 'Utility'],
-            type: 'pie'
-        }];
-
-        var layout = {
-            title: 'title'
-
-        };
-
-        Plotly.newPlot('pie_' + id, data, layout);
-    }
-
-    function barChart(id, x, y, title){
-        console.log(x);
-        console.log(y);
+    function barChart(id, x, y, title, xlable, ylable){
         var data = [
             {
                 x: x,
@@ -31,10 +14,57 @@ $(function(){
             }
         ];
         var layout = {
-            title: title
+            title: title,
+            yaxis: {
+                title: ylable
+            },
+            xaxis:{
+                title: xlable
+            }
 
         };
         Plotly.newPlot('bar_' + id, data, layout);
+    }
+
+    function lineChart(id, x, y, title, xlable, ylable){
+        var trace1 = {
+            x: x,
+            y: y,
+            type: 'scatter'
+        };
+        var layout = {
+            title: title,
+            yaxis: {
+                title: ylable
+            },
+            xaxis:{
+                title: xlable
+            }
+        };
+        var data = [trace1];
+
+        Plotly.newPlot('line_' + id, data, layout);
+    }
+
+    function scatterChart(id, x, y, title, xlable, ylable){
+        var trace1 = {
+            x: x,
+            y: y,
+            mode: 'markers',
+            type: 'scatter'
+        };
+        var layout = {
+            title: title,
+            yaxis: {
+                title: ylable
+            },
+            xaxis:{
+                title: xlable
+            }
+        };
+        var data = [trace1];
+
+        Plotly.newPlot('scatter_' + id, data, layout);
     }
 
     function renderPloty() {
@@ -42,13 +72,34 @@ $(function(){
             url: '/dashboard',
             type: 'post',
             dataType: 'json',
+            async: false,
             data: {user:'just demo haha', id:87},
             success : function(datas) {
                 //<div class="chart wow zoomIn" id="pie_1" ></div>
                 $.each(datas, function( i, data ) {
-                    $( "#plotly" ).append( "<div class='chart wow zoomIn' id='" + data.type + "_" + data.id +"'></div>" );
+                    $( "#plotly" ).append( "<div class='chart wow zoomIn' id='" + data.type + "_" + data.id +"'>" +
+                        "<button type='button' class='btn remove_btn pull-right btn-info btn-sm' value="+data.id+">" +
+                        "<span class='glyphicon glyphicon-remove'></span> Rwmove" +
+                        "</button>" +
+                        "<button type='button' class='btn modify_btn pull-right btn-info btn-sm' value="+data.id+">" +
+                        "<span class='glyphicon glyphicon-pencil'></span> modify" +
+                        "</button>" +
+                        " </div>" );
+                    /*
+                    *
+                    * 				<div class="chart wow zoomIn" id="test" >
+                     <button type="button" class="btn remove_btn pull-right btn-info btn-sm" value="1">
+                     <span class="glyphicon glyphicon-remove"></span> Remove
+                     </button>
+                     <button type="button" class="btn modify_btn pull-right btn-info btn-sm">
+                     <span class="glyphicon glyphicon-pencil"></span> modify
+                     </button>
+                     </div>
+                    *
+                    *
+                    *
+                    * */
                     $("#"+data.type+"_"+data.id).css({top: data.top, left:data.left, width:data.width, height:data.height});
-                    console.log(data.name);
 
                     $.ajax({
                         url: '/data/' + data.name ,
@@ -58,21 +109,22 @@ $(function(){
                             dataDetails = eval(dataDetails);
                             var x = [];
                             var y = [];
+                            var columns = eval(data.columns);
                             $.each(dataDetails, function( i, dataDetail ) {
-                                var columns = eval(data.columns);
-                                console.log(columns[0]);
+
                                 x.push(dataDetail[columns[0]]);
                                 y.push(dataDetail[columns[1]]);
                             });
-                            console.log(x);
-                            console.log(y);
-                            if (data.type == 'pie') {
-                                pieChart(data.id, x, y);
-                            }
-                            else if(data.type == 'bar'){
-                                barChart(data.id, x, y, data.name);
-                            }
 
+                            if(data.type == 'bar'){
+                                barChart(data.id, x, y, data.name, columns[0], columns[1]);
+                            }
+                            else if(data.type == 'line'){
+                                lineChart(data.id, x, y, data.name, columns[0], columns[1]);
+                            }
+                            else if(data.type == 'scatter'){
+                                scatterChart(data.id, x, y, data.name, columns[0], columns[1]);
+                            }
                         }
                     });
                 });
@@ -100,10 +152,43 @@ $(function(){
                     },
                     cancel: ".coor"
                 });
+
             }
         });
     }
 
     renderPloty();
 
+    $( ".remove_btn" ).click(function() {
+        /*$.ajax({
+            url: '/dataVisual',
+            type: 'delete',
+            dataType: 'json',
+            async: false,
+            data: {id: $(this).val()},
+            success: function (datas) {
+
+            }
+        });*/
+        //location.reload();
+        console.log($(this).val());
+    });
+
+    $( ".modify_btn" ).click(function() {
+
+    });
+
+
+
+    var data = [
+        {
+            x: ['giraffes', 'orangutans', 'monkeys'],
+            y: [20, 14, 23],
+            type: 'bar'
+        }
+    ];
+
+
+    //$("#test").css({top: 0, left:0, width:480, height:400});
+    //Plotly.newPlot('test', data);
 });
