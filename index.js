49694d55a2870f6de6acf88c5b45b037794619f8
YANG, SHUN-YU <mywonderemailyoung@gmail.com>
2017-05-27 11:30:27 +0800

    add sql add static module

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const Router = require('koa-router')
 const route = require('./routers/route')
 const logger = require('koa-logger')
 const bodyparser = require('koa-bodyparser')
+const static = require('koa-static')
 const path = require('path')
 const config = require('./config')
 
+const staticPath = './static'
 const app = new Koa()
 app.use(logger())
 app.use(bodyparser())
+app.use(static(
+    path.join(__dirname, staticPath)
+))
 app.use(views(path.join(__dirname, './views'), {
     extension: 'ejs'
 }))
