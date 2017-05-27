01afb7ebc35ec0444185775c7a3655ef2e57b8af
sleep <wjs09211>
2017-05-28 03:07:50 +0800

    datavisual

--- controllers/dataVisual.js
+++ controllers/dataVisual.js
@@ -2,7 +2,8 @@ const views = require('koa-views')
 const dataAPI = require('./data')
 let dataVisual = async(ctx) => {
     await dataAPI.description(ctx)
-    await ctx.render('dataVisual', data=JSON.parse(ctx.body))
+    console.log(ctx.params.name)
+    await ctx.render('dataVisual', data=JSON.parse(ctx.body),  myname=ctx.params.name)
 }
 
 module.exports = dataVisual
