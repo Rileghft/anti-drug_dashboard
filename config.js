882a09d2c3321fa6977268cd1ba37185a083c3fb
楊舜宇 <mywonderemailyoung@gmail.com>
2017-06-10 11:55:21 +0800

    update config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     port: 8080,
     db: {
-        host: 'rileghft.ddns.net',
+        host: 'localhost',
         user: 'hackthon',
         password: 'hacker@yzu',
         database: 'hackthon_data'
